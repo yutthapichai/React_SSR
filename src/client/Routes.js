098995ffd67115174a272a45e6f1bba8df6e19a5
Yutthapichai <cancel_go@hotmail.com>
2018-08-26 23:43:06 +0700

    Create store, reducers for server, client

--- src/client/Routes.js
+++ src/client/Routes.js
@@ -2,12 +2,13 @@ import React from 'react'
 import { Route } from 'react-router-dom'
 
 import Home from './component/Home'
+import UsersList from './component/UsersList'
 
 export default () => {
   return (
     <div>
       <Route exact path="/" component={Home} />
-      <Route path='/hi' component={() => 'Hi'} />
+      <Route path='/users' component={UsersList} />
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -4,15 +4,19 @@ const React = require('react')
 const renderToString = require('react-dom/server').renderToString
 const Home = require('./client/component/Home').default
 */
+import 'babel-polyfill' // acait syntack
 import express from 'express'
 import renderer from './helpers/renderer'
+import createStore from './helpers/createStore'
 
 const app = express();
 
 app.use(express.static('public'))
 
 app.get('*', (req, res) => {
-  res.send(renderer(req))
+  const store = createStore()
+  //load date to stoer
+  res.send(renderer(req, store))
 })
 
 app.listen(2000, () => {
